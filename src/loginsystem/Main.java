7061fc8c08027ece0479e50ed5d2a94921a6ad90
Florian <letszockerlp2@gmail.com>
2021-10-04 16:01:19 +0200

    Initial Commit

---
+++ src/loginsystem/Main.java
@@ -0,0 +1,20 @@
+package loginsystem;
+
+/*
+ * Loginsystem
+ * 
+ * simple login screen with GUI
+ * able to create and login to accounts
+ * 
+ * Date: 4.10.2021
+ * Author: Florian [derFlo19]
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
